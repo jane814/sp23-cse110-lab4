8d3d1b63033ebe5835cba65413d953391681953f
jane814 <63211137+jane814@users.noreply.github.com>
2023-04-30 19:37:44 -0700

    Update part2-question18.js

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -1,6 +1,6 @@
-let d = new Date();
-let time = d.toLocaleTimeString();
 function printTime(){
+  let d = new Date();
+  let time = d.toLocaleTimeString();
   console.log(time);
 }
 setInterval(printTime,1000);
